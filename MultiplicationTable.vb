80266585ecf5e337bddd134d30f670d185f73b7e
Brayden Peck <brayden.peck2@gmail.com>
2021-02-05 08:26:43 -0700

    Add project files.

---
+++ MultiplicationTable.vb
@@ -0,0 +1,22 @@
+﻿Module MultiplicationTable
+
+    Sub Main()
+        Dim userInput As String
+        Dim firstDimension As Integer
+        Dim secondDimension As Integer
+        Dim problem As Boolean = True
+
+        While problem = True And userInput <> "Q" And userInput <> "q"
+            Try
+                Console.WriteLine("please enter the first dimension of the table")
+                userInput = Console.ReadLine()
+                firstDimension = CInt(userInput)
+                problem = False
+            Catch ex As Exception
+                Console.WriteLine("please enter a whole number")
+            End Try
+        End While
+
+    End Sub
+
+End Module
---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("MultiplicationTable")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("MultiplicationTable")>
+<Assembly: AssemblyCopyright("Copyright ©  2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("f9de01bf-5084-41ca-a703-73be3b0f4c1d")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
